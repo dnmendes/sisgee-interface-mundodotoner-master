cb9d9b97819db9be854ead35d6bd891f3eb20a1b
Daniel Nunes Mendes <133063955+dnmendes@users.noreply.github.com>
2023-06-29 18:34:56 -0300

    Update Home.jsx

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,7 +1,8 @@
 const Home = () => (
     <div>
         <h1>REGISTRO DE PRODUTOS</h1>
-        <h2>ioioiioioioioiioio</h2>
+        <h2>MUNDO DO TONER</h2>
+        <h5>A PRIMEIRA IMPRESSÃO É A QUE FICA!</h5>
     </div>
 );
 export default Home;
